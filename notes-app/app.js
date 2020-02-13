e4f25d25be887b522ed900bf580d4c5664e05ccd
Yulian Ashikov <u.ashikov@abv.bg>
2020-02-13 23:17:49 +0200

    Importing own files and using the utils from them.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,7 @@
 const fs = require('fs');
+const getNotes = require('./notes');
 
 fs.appendFileSync("notes.txt", 'This is new text, appended to the file.');
+
+var notes = getNotes();
+console.log(notes);
---
+++ notes-app/notes.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+function getNotes() {
+    return "Your notes are very interesting...";
+}
+
+module.exports = getNotes;
