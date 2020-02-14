2e50e5e99796bc2aa260b54f54802971f80e73ba
Yulian Ashikov <u.ashikov@abv.bg>
2020-02-14 21:11:14 +0200

    Installed and used chalk library for colorizing text.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const getNotes = require('./notes');
+const chalk = require('chalk');
 
 fs.appendFileSync("notes.txt", 'This is new text, appended to the file.');
 
 var notes = getNotes();
-console.log(notes);
+
+console.log(chalk.red.underline.bgBlack(notes));
