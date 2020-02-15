b4ff408cdba3c0f64462641f99aaee07f3f4f12d
Yulian Ashikov <u.ashikov@abv.bg>
2020-02-15 21:33:35 +0200

    Added yargs library. Created simple commands - add note, list notes and read a note.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,42 @@
-const fs = require('fs');
 const getNotes = require('./notes');
 const chalk = require('chalk');
+const yargs = require('yargs');
 
-fs.appendFileSync("notes.txt", 'This is new text, appended to the file.');
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note.',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Adding a new note with title ' + argv.title);
+        console.log('Adding a new note with body ' + argv.body);
+    }
+});
 
-var notes = getNotes();
+yargs.command({
+    command: 'list',
+    describe: 'Listing all notes.',
+    handler: function () {
+        console.log('Listing all the notes..')
+    }
+});
 
-console.log(chalk.red.underline.bgBlack(notes));
+yargs.command({
+    command: 'read',
+    describe: 'Reading a note.',
+    handler: function () {
+        console.log('Reading a note...');
+    }
+});
+
+yargs.parse();
