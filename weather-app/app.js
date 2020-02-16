8cda77aa21e637d5fe208216be4c76be08c268e7
Yulian Ashikov <u.ashikov@abv.bg>
2020-02-16 21:45:00 +0200

    Getting the geocoding coordinates for particular city.

--- weather-app/app.js
+++ weather-app/app.js
@@ -14,3 +14,16 @@ instance.get('/37.8267,-122.4233')
     .catch(function (error) {
         console.error(error);
     });
+
+instance.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoieWFzaGlrb3YiLCJhIjoiY2s2cGYwN253MHRpMzNlcXA5ZndoaTZyZyJ9._HVCLkxwuAOPDAq5liXKKA')
+    .then(function (response) {
+        var geoData = response.data;
+
+        if (geoData && geoData.features.length > 0) {
+            var coordinates = geoData.features[0];
+            console.log(coordinates.place_name + ', Longitude: ' + coordinates.center[0] + ', Latitude: ' + coordinates.center[1]);
+        }
+    })
+    .catch(function (error) {
+        console.error(error);
+    })
