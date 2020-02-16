42053df79872b75f5f9a6a621f36e54c83d72092
Yulian Ashikov <u.ashikov@abv.bg>
2020-02-16 22:09:06 +0200

    Added error handling.

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,7 +4,7 @@ var instance = axios.create({
     baseURL: 'https://api.darksky.net/forecast/68b5a5e02385b80fe158514ab506ff22'
 });
 
-instance.get('/37.8267,-122.4233')
+instance.get('/37.8267, 111.22')
     .then(function (response) {
         if (response) {
             var currentlyForecast = response.data.currently;
@@ -12,18 +12,28 @@ instance.get('/37.8267,-122.4233')
         }
     })
     .catch(function (error) {
-        console.error(error);
+        if (!error.response) {
+            console.log('Unable to connect to the DarkSky API.');
+        } else {
+            console.log(error.response.data.error);
+        }
     });
 
-instance.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoieWFzaGlrb3YiLCJhIjoiY2s2cGYwN253MHRpMzNlcXA5ZndoaTZyZyJ9._HVCLkxwuAOPDAq5liXKKA')
+instance.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles1111.json?access_token=pk.eyJ1IjoieWFzaGlrb3YiLCJhIjoiY2s2cGYwN253MHRpMzNlcXA5ZndoaTZyZyJ9._HVCLkxwuAOPDAq5liXKKA')
     .then(function (response) {
         var geoData = response.data;
 
         if (geoData && geoData.features.length > 0) {
             var coordinates = geoData.features[0];
             console.log(coordinates.place_name + ', Longitude: ' + coordinates.center[0] + ', Latitude: ' + coordinates.center[1]);
+        } else {
+            console.log('Place not found!');
         }
     })
     .catch(function (error) {
-        console.error(error);
+        if (!error.response) {
+            console.log('Unable to connect to the MaxBox API.');
+        } else {
+            console.log(error.response.data.message);
+        }
     })
