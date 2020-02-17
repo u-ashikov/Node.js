59a5c75a50a3fe5c87dc9f8ef2806fb9b19dbbb3
Yulian Ashikov <u.ashikov@abv.bg>
2020-02-17 23:01:41 +0200

    Extracted getting the coordinates and the forecast as utility functions.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,39 +1,18 @@
-const axios = require('axios');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-var instance = axios.create({
-    baseURL: 'https://api.darksky.net/forecast/68b5a5e02385b80fe158514ab506ff22'
+geocode('Los Angeles', (error, response) => {
+    if (error) {
+        console.log('Error', error);
+    } else {
+        console.log(response);
+    }
 });
 
-instance.get('/37.8267, 111.22')
-    .then(function (response) {
-        if (response) {
-            var currentlyForecast = response.data.currently;
-            console.log('It is currently ' + currentlyForecast.temperature + ' degrees out. There is a ' + currentlyForecast.precipProbability + '% chance of rain.');
-        }
-    })
-    .catch(function (error) {
-        if (!error.response) {
-            console.log('Unable to connect to the DarkSky API.');
-        } else {
-            console.log(error.response.data.error);
-        }
-    });
-
-instance.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles1111.json?access_token=pk.eyJ1IjoieWFzaGlrb3YiLCJhIjoiY2s2cGYwN253MHRpMzNlcXA5ZndoaTZyZyJ9._HVCLkxwuAOPDAq5liXKKA')
-    .then(function (response) {
-        var geoData = response.data;
-
-        if (geoData && geoData.features.length > 0) {
-            var coordinates = geoData.features[0];
-            console.log(coordinates.place_name + ', Longitude: ' + coordinates.center[0] + ', Latitude: ' + coordinates.center[1]);
-        } else {
-            console.log('Place not found!');
-        }
-    })
-    .catch(function (error) {
-        if (!error.response) {
-            console.log('Unable to connect to the MaxBox API.');
-        } else {
-            console.log(error.response.data.message);
-        }
-    })
+forecast('37.8267', '111.22', (error, response) => {
+    if (error) {
+        console.log('Error', error);
+    } else {
+        console.log(response);
+    }
+});
---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+
+var instance = axios.create({
+    baseURL: 'https://api.darksky.net/forecast/68b5a5e02385b80fe158514ab506ff22'
+});
+
+function getForecast(latitude, longitude, callback) {
+    if (!latitude || !longitude) {
+        callback('Error, please enter valid coordinates.', undefined);
+    }
+
+    instance.get('/' + latitude + ',' + longitude)
+        .then(function (response) {
+            if (response) {
+                var currentlyForecast = response.data.currently;
+                callback(undefined, '' + 'It is currently ' + currentlyForecast.temperature + ' degrees out. There is a ' + currentlyForecast.precipProbability + '% chance of rain.', undefined);
+            }
+        })
+        .catch(function (error) {
+            if (!error.response) {
+                callback('Unable to connect to the DarkSky API.', undefined);
+            } else {
+                console.log(error.response.data.error, undefined);
+            }
+        });
+}
+
+module.exports = getForecast;
