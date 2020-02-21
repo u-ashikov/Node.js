f876a46da38b06a827d7fbe71b4a3325a1ddafb1
Yulian Ashikov <u.ashikov@abv.bg>
2020-02-21 18:32:54 +0200

    Changing from static html files to views using handlebars.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -8,6 +8,33 @@ const publicDirectory = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectory));
 
+app.set('view engine', 'hbs');
+
+app.get('', (req, res) => {
+    var model = {
+        title: 'Weather Application'
+    };
+
+    res.render('index', model);
+});
+
+app.get('/about', (req, res) => {
+    var model = {
+        title: 'About Me',
+        name: 'Random cat name'
+    };
+
+    res.render('about', model);
+});
+
+app.get('/help', (req, res) => {
+    var model = {
+        message: 'Here you can find (eventually) help information.'
+    }
+
+    res.render('help', model);
+});
+
 app.get('/weather', (req, res) => {
     var weatherInfo = {
         location: 'Sofia',
