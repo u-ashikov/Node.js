7db3d748cc565b09a2d5a865d45996ced4bf19ca
Yulian Ashikov <u.ashikov@abv.bg>
2020-03-08 15:13:37 +0200

    Inserting single and multiple users.

--- task-manager/index.js
+++ task-manager/index.js
@@ -8,7 +8,35 @@ const databaseName = 'task-manager';
 MongoClient.connect(connectionURL, { 'useNewUrlParser': true }, (error, client) => {
    if (error) {
        console.log('Unable to connect to the database!');
-   } else {
-       console.log('Successfully connected!');
    }
+
+   const db = client.db(databaseName);
+
+   db.collection('users').insertOne({
+       name: 'Gosho',
+       age: 17
+   }, (error, result) => {
+       if (error) {
+           return console.log('Unable to insert document!');
+       }
+
+       console.log(result.ops);
+   });
+
+   db.collection('users').insertMany([
+       {
+        name: 'Stavri',
+        age: 19 
+       },
+       {
+        name: 'Mincho',
+        age: 21 
+       }
+   ], (error, result) => {
+       if (error) {
+           return console.log('Unable to insert documents!');
+       }
+
+       console.log(result.ops);
+   })
 });
