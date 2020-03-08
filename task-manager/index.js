d9ed232cb9315ce384b78969b057948399ebacd6
Yulian Ashikov <u.ashikov@abv.bg>
2020-03-08 16:19:22 +0200

    Reading data from the database - finding single and multiple documents by different criteria.

--- task-manager/index.js
+++ task-manager/index.js
@@ -4,6 +4,7 @@ const MongoClient = mongodb.MongoClient;
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
+const ObjectID = mongodb.ObjectId;
 
 MongoClient.connect(connectionURL, { 'useNewUrlParser': true }, (error, client) => {
     if (error) {
@@ -15,6 +16,12 @@ MongoClient.connect(connectionURL, { 'useNewUrlParser': true }, (error, client) 
     //insertUser(db);
     //insertUsers(db);
     //insertTasks(db);
+
+    //findUserByName(db, 'Stavri');
+    //findUsersByAge(db, 17);
+
+    //findTaskById(db, '5e64effe4c45de2b18bd63e9');
+    findIncompletedTasks(db, false);
 });
 
 function insertUser(db) {
@@ -71,3 +78,59 @@ function insertTasks(db) {
         console.log(result.ops);
     });
 };
+
+function findUserByName(db, name) {
+    db.collection('users').findOne({ name }, (error, user) => {
+        if (error) {
+            return console.log('Unable to fetch data!');
+        }
+
+        if (!user) {
+            return console.log('User not found!');
+        }
+
+        console.log(user);
+    });
+}
+
+function findUsersByAge(db, age) {
+    db.collection('users').find({ age }).toArray((error, users) => {
+        if (error) {
+            return console.log('Unable to fetch data!');
+        }
+
+        if (!users || users.length == 0) {
+            return console.log('No users found!');
+        }
+
+        console.log(users);
+    });
+}
+
+function findTaskById(db, id) {
+    db.collection('tasks').findOne({_id : ObjectID(id)}, (error, task) => {
+        if (error) {
+            return console.log('Unable to fetch data!');
+        }
+
+        if (!task) {
+            return console.log('Task not found!');
+        }
+
+        console.log(task);
+    });
+}
+
+function findIncompletedTasks(db, isCompleted) {
+    db.collection('tasks').find({ completed: isCompleted }).toArray((error, tasks) => {
+        if (error) {
+            return console.log('Unable to fetch data!');
+        }
+
+        if (!tasks || tasks.length == 0) {
+            return console.log('No tasks found!');
+        }
+
+        console.log(tasks);
+    });
+}
