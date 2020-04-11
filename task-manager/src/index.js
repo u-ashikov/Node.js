2ef0aa171f0b00a8f267cc8c6a8e8f4cc7792ed6
Yulian Ashikov <u.ashikov@abv.bg>
2020-04-12 00:02:21 +0300

    Added endpoints for creating tasks and users.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,11 +1,40 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const User = require('./db/models/user');
+const Task = require('./db/models/task');
+require('./db/mongoose');
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello stranger, this is Task Manager API!');
+});
+
+app.post('/user', (req, res) => {
+    const user = new User(req.body);
+
+    user.save().then(() => {
+        res.status(201);
+        res.send('User created!');
+    })
+    .catch((err) => {
+        res.status(400);
+        res.send(err);
+    });
+});
+
+app.post('/task', (req, res) => {
+    const task = new Task(req.body);
+
+    task.save().then(() => {
+        res.status(201);
+        res.send('Task created!');
+    })
+    .catch((err) => {
+        res.status(400);
+        res.send(err);
+    });
 });
 
 app.listen(port, () => { 
