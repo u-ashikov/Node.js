aaa327c26dedaf7fc7ddcf17da5d4c95b4754d3e
Yulian Ashikov <u.ashikov@abv.bg>
2020-04-11 22:04:42 +0300

    Added validations to the task model.

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -33,7 +33,7 @@ const User = mongoose.model('User', {
         type: String,
         minlength: [6, "The password length must be greater or equal to 6."],
         validate(value) {
-            if (value.includes('password')) {
+            if (value.toLowerCase().includes('password')) {
                 throw new Error('The password cannot contain password.');
             }
         },
@@ -43,35 +43,37 @@ const User = mongoose.model('User', {
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: [true, 'The description is required.']
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 });
 
-const user = new User({
-    name: 'Yulian',
-    age: 31,
-    email: 'ASHIK@ashi.com    ',
-    password: 'asddsa'
-});
-
-// const task = new Task({
-//     description: 'This is my first task',
-//     completed: false
+// const user = new User({
+//     name: 'Yulian',
+//     age: 31,
+//     email: 'ASHIK@ashi.com    ',
+//     password: 'asddsa'
 // });
 
-user.save().then(() => {
-    console.log('User ' + user.name + ' stored in the database!')
-})
-.catch((err) => {
-    console.log('Error', err);
-});
-
-// task.save().then(() => {
-//     console.log('Task created!');
+// user.save().then(() => {
+//     console.log('User ' + user.name + ' stored in the database!')
 // })
 // .catch((err) => {
 //     console.log('Error', err);
 // });
+
+const task = new Task({
+    description: 'Learning some new stuff'
+    // completed: false
+});
+
+task.save().then(() => {
+    console.log('Task created!');
+})
+.catch((err) => {
+    console.log('Error', err);
+});
