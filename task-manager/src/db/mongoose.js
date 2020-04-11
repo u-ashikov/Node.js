136bf4305847a1c2340f9994dec4bd7092e010a7
Yulian Ashikov <u.ashikov@abv.bg>
2020-04-11 21:59:36 +0300

    Added validations to the user model. Included validator library.

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,13 +1,43 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://localhost:27017/task-manager', { useNewUrlParser: true, useCreateIndex: true });
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: [true, 'User name is required.'],
+        trim: true
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('The age must be a positive number.');
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: [true, 'The email is required.'],
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('The email is not valid.');
+            }
+        },
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        minlength: [6, "The password length must be greater or equal to 6."],
+        validate(value) {
+            if (value.includes('password')) {
+                throw new Error('The password cannot contain password.');
+            }
+        },
+        trim: true
     }
 });
 
@@ -22,24 +52,26 @@ const Task = mongoose.model('Task', {
 
 const user = new User({
     name: 'Yulian',
-    age: 31
+    age: 31,
+    email: 'ASHIK@ashi.com    ',
+    password: 'asddsa'
 });
 
-const task = new Task({
-    description: 'This is my first task',
-    completed: false
-});
-
-// user.save().then(() => {
-//     console.log('User ' + user.name + ' stored in the database!')
-// })
-// .catch((err) => {
-//     console.log('Error', err);
+// const task = new Task({
+//     description: 'This is my first task',
+//     completed: false
 // });
 
-task.save().then(() => {
-    console.log('Task created!');
+user.save().then(() => {
+    console.log('User ' + user.name + ' stored in the database!')
 })
 .catch((err) => {
     console.log('Error', err);
 });
+
+// task.save().then(() => {
+//     console.log('Task created!');
+// })
+// .catch((err) => {
+//     console.log('Error', err);
+// });
