f69ca9c701637b562a01f47112d3c50c9f0235ec
Yulian Ashikov <u.ashikov@abv.bg>
2020-04-11 20:59:39 +0300

    Created model for task.

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -11,13 +11,34 @@ const User = mongoose.model('User', {
     }
 });
 
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+});
+
 const user = new User({
     name: 'Yulian',
     age: 31
 });
 
-user.save().then(() => {
-    console.log('User ' + user.name + ' stored in the database!')
+const task = new Task({
+    description: 'This is my first task',
+    completed: false
+});
+
+// user.save().then(() => {
+//     console.log('User ' + user.name + ' stored in the database!')
+// })
+// .catch((err) => {
+//     console.log('Error', err);
+// });
+
+task.save().then(() => {
+    console.log('Task created!');
 })
 .catch((err) => {
     console.log('Error', err);
