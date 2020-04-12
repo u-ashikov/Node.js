07c17364f9d5689247e4eb73487a4e68a436b582
Yulian Ashikov <u.ashikov@abv.bg>
2020-04-12 18:35:32 +0300

    Hashing user's passwords.

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -53,9 +53,13 @@ router.patch('/tasks/:id', async (req,res) => {
     } 
 
     try {
-        const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true});
+        const task = await Task.findById(req.params.id);
 
-        if (!updatedTask) {
+        updates.forEach((update) => task[update] = req.body[update]);
+
+        await task.save();
+
+        if (!task) {
             return res.status(404).send();
         }
 
@@ -77,6 +81,6 @@ router.delete('/tasks/:id', async (req, res) => {
     } catch (err) {
         res.status(500).send(err);
     }
-})
+});
 
 module.exports = router;
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -51,9 +51,13 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        const user = await User.findById(req.params.id);
+        
+        updates.forEach((update) => user[update] = req.body[update]);
 
-        if (!updatedUser) {
+        await user.save();
+
+        if (!user) {
             return res.status(404).send();
         }
 
