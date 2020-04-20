06f9398513c9fdb9c9952a8af7ce281ad4a275b6
Yulian Ashikov <u.ashikov@abv.bg>
2020-04-20 23:28:37 +0300

    Added cat model. Refactored the breed model.

--- cat-shelter/src/models/breed.js
+++ cat-shelter/src/models/breed.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('../data/mongoose');
 
 const breedSchema = new mongoose.Schema({
     name: {
---
+++ cat-shelter/src/models/cat.js
@@ -0,0 +1,25 @@
+const mongoose = require('../data/mongoose');
+
+const catSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    breed: {
+        type: 'ObjectId',
+        ref: 'Breed',
+        required: true
+    }
+});
+
+const Cat = mongoose.Model('Cat', catSchema);
+
+module.exports = Cat;
